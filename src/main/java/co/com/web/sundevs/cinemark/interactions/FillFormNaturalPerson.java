1df4ee543d8a4dd145fa46e62509306df74adec1
Daniel Henao <dhenao693@gmail.com>
2024-01-25 09:30:19 -0500

    change option of list

--- src/main/java/co/com/web/sundevs/cinemark/interactions/FillFormNaturalPerson.java
+++ src/main/java/co/com/web/sundevs/cinemark/interactions/FillFormNaturalPerson.java
@@ -10,6 +10,8 @@ import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
 import net.serenitybdd.screenplay.actions.Scroll;
 
+import static co.com.web.sundevs.cinemark.userinterfaces.General.BTN_OPTION_FROM_LIST_1;
+import static co.com.web.sundevs.cinemark.userinterfaces.General.BTN_OPTION_FROM_LIST_3;
 import static co.com.web.sundevs.cinemark.userinterfaces.Register.*;
 
 
@@ -21,11 +23,11 @@ public class FillFormNaturalPerson implements Interaction {
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
                 Click.on(LST_DOCUMENT_TYPE),
-                Click.on(General.BTN_OPTION_FROM_LIST_1.of(user.getDocumentType())),
+                Click.on(BTN_OPTION_FROM_LIST_1.of(user.getDocumentType())),
                 Enter.theValue(user.getName()).into(LBL_NAME),
                 Enter.theValue(user.getLastName()).into(LBL_LASTNAME),
                 Click.on(LST_GENDER),
-                Click.on(General.BTN_OPTION_FROM_LIST_1.of(user.getGender())),
+                Click.on(BTN_OPTION_FROM_LIST_1.of(user.getGender())),
                 Enter.theValue(user.getDocumentNumber()).into(LBL_DOCUMENT_NUMBER),
                 SelectBirthdate.of(user.getBirthdate()),
                 Enter.theValue(user.getPhone()).into(LBL_PHONE),
@@ -33,12 +35,13 @@ public class FillFormNaturalPerson implements Interaction {
                 Scroll.to(BTN_ACCEPT_TERMS),
                 Enter.theValue(user.getEmail()).into(LBL_EMAIL),
                 Enter.theValue(user.getEmail()).into(LBL_RETRY_EMAIL),
+                Click.on(LBL_PASSWORD),
                 Enter.theValue(user.getPassword()).into(LBL_PASSWORD),
+                Click.on(LBL_PASSWORD),
                 Enter.theValue(user.getPassword()).into(LBL_RETRY_PASSWORD),
-                Click.on(LST_CITY),
-                Click.on(General.BTN_OPTION_FROM_LIST_1.of(user.getCity())),
+                Click.on(LST_CITY),Click.on(BTN_OPTION_FROM_LIST_3.of(user.getCity())),
                 Click.on(LST_PREFERRED_THEATER),
-                Click.on(General.BTN_OPTION_FROM_LIST_1.of(user.getPreferredTheater())),
+                Click.on(BTN_OPTION_FROM_LIST_3.of(user.getPreferredTheater())),
                 Click.on(BTN_ACCEPT_TERMS),
                 Click.on(BTN_CREATE_ACCOUNT)
         );
